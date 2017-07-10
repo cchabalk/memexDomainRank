cb53ac39ba149d0af27ca227df5d174a79e13a96
user1 <christopher.chabalko@soteradefense.com>
2017-07-10 15:22:30 -0400

    Added additional steps in the pipeline

---
+++ 020countLinks.py
@@ -0,0 +1,332 @@
+import os
+from os import listdir
+from os.path import isfile, join, normpath
+import sys
+import gzip
+import ujson
+import urllib
+import time
+import gc
+from multiprocessing import Pool
+import signal
+
+def getGzFilesInDirectory(inDir):
+    inDir = normpath(inDir)
+    onlyFiles = [join(inDir, f) for f in listdir(inDir) if (isfile(join(inDir, f)) and ((f[-1] == 'z')))]
+    return onlyFiles
+
+def getCsvFilesInDirectory(inDir):
+    inDir = normpath(inDir)
+    onlyFiles = [join(inDir, f) for f in listdir(inDir) if (isfile(join(inDir, f)) and ((f[-1] == 'v')))]
+    return onlyFiles
+
+def getNonProcessedFiles(gzDir, csvDir):
+    gzFiles = getGzFilesInDirectory(gzDir)
+    csvFiles = getCsvFilesInDirectory(csvDir)
+
+    #filter files
+    gzFilesFiltered = []
+
+    #convert to csv filename and the drop extension
+
+    for file in gzFiles:
+        csvFile = convertGZNametoCSVName(file)
+
+        #N = len(file)
+        #ix = N - file[::-1].find('.') - 1  # find last '.'
+        #file = file[0:ix]
+
+        # Find last again
+        #N = len(file)
+        #if '.' in file:
+        #    ix = N - file[::-1].find('.') - 1  # find last '.'
+        #    file = file[0:ix]
+        gzFilesFiltered.append(csvFile[:-4])
+
+    csvFilesFiltered = []
+    for file in csvFiles:
+        #N = len(file)
+        #ix = N - file[::-1].find('.') - 1  # find last '.'
+        #file = file[0:ix]
+        #csvFilesFiltered.append(file)
+        csvFilesFiltered.append(file[:-4])
+
+    #do the compairison
+    filesToBeProcessed = []
+    for k1,f1 in enumerate(gzFilesFiltered):
+        addToList = True
+        for f2 in csvFilesFiltered:
+            if f1 == f2:
+                addToList = False
+        if addToList == True:
+            filesToBeProcessed.append(gzFiles[k1])
+
+    return filesToBeProcessed
+
+def getNonProcessedFilesVerified(gzDir, csvDir,parsingType):
+    nStart = 0
+    nSuccess = 0
+    nFail = 0
+    gzFiles = getGzFilesInDirectory(gzDir)
+    csvFiles = getCsvFilesInDirectory(csvDir)
+
+
+    filesToBeProcessed = []  #list of files to be processed
+
+    for file in gzFiles[nStart:]:
+        #open a gz file, get the final parent url
+        nGZ = 1 #default value
+        try:
+            with gzip.open(file, "rb") as f:
+                gzUniqueSet = []
+                nGZ = 0
+                data = ' ' #initialize data to be read later
+
+                for line in f:
+                    data = line  # Read all the lines serially
+                    t1 = ujson.loads(data)['parentURL']
+                    t2 = reparse(t1,parsingType)
+                    gzUniqueSet.append(t2)
+            gzUniqueSet = set(gzUniqueSet)
+            nGZ = len(gzUniqueSet)
+        except:
+            pass
+
+
+        #open the corrisponding csv file, get the final parent url
+        nCSV = 0
+        csvFile = convertGZNametoCSVName(file)
+        csvParentURL = ' ' #will be populated later
+        try:
+            with open(csvFile, "rb") as f:
+                csvUniqueSet = []
+                nCSV = 0
+                for line in f:
+                    csvParentURL = line.split(',')[0]  #get the first csv value
+                    csvUniqueSet.append(csvParentURL)
+                csvUniqueSet = set(csvUniqueSet)  #convert to set
+                nCSV = len(csvUniqueSet)
+
+        except:
+            print csvFile, 'not found'
+
+        #if they are the same, the csv file is complete, and the fileName should be removed from the queue
+        percentDifference = 100
+        percentDifference = float((nGZ-nCSV)/float(nGZ)*100.)
+        print 'nGZ:', nGZ, 'nCSV:', nCSV, 'pDif:', percentDifference
+
+        if percentDifference<=6.:
+            nSuccess += 1
+            #don't do anything
+
+            print file, ' processed successfully '
+            print 'nSuccess', nSuccess
+        else:
+            nFail += 1
+            print file, ' requires reprocessing '
+            print 'nFail', nFail
+            filesToBeProcessed.append(file)
+
+        with open('logFile2.csv','a+') as f:
+            output = str(nSuccess+nFail+nStart) + ',' + file + ',nGZ,' + str(nGZ) + ',nCSV,' + str(nCSV) + ',pDif,' + str(percentDifference) + '\n'
+            f.write(output)
+
+    return filesToBeProcessed
+
+
+def convertGZNametoCSVName(fileName):
+    N = len(fileName)
+    ix = N - fileName[::-1].find('.') - 1  # find last '.'
+    csvFile = fileName[0:ix]
+
+    # Find last again
+    N = len(csvFile)
+    if '.' in csvFile:
+        ix = N - csvFile[::-1].find('.') - 1  # find last '.'
+        csvFile = csvFile[0:ix]
+
+    csvFile = csvFile + '.csv'
+    csvFile = os.path.normpath(csvFile)
+    return csvFile
+
+def writeDictToCsv(fileName, linksDict):
+
+    # save dict to file
+
+    (p,f) = os.path.split(fileName) #get (path, file)
+    p = os.path.join(p,os.path.normpath('counted'))  #concatenate counted to path
+    countedFile = os.path.join(p,f) #make fileName to store file in "counted" directory
+
+    #create directory with COUNTED files
+    #this files contain parents/child links with COUNTS rather than
+    #individual children
+    ensure_dir(countedFile)
+
+    try:
+        with gzip.open(countedFile, 'w+') as fp:
+            fp.write(ujson.dumps(linksDict))
+    except:
+        print "unable to write"
+
+
+    print "wrote " + countedFile
+    return
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+
+
+
+def doTheWork(input):
+
+
+    linksDict = {}
+    fileName = input[0]
+    with gzip.open(fileName) as fp:
+
+        linesCount = 0
+        tStart = time.time()
+        # for x in range(0,5):
+        # import pdb; pdb.set_trace()
+        # ll = fp.readlines()
+
+        for line in fp:
+            linesCount += 1
+            pURL = []
+
+            try:
+                data = ujson.loads(line)
+                pURL = data['parentURL']
+            except:
+                print 'failed'
+            if (len(pURL) > 1):
+                targetDict = linksDict.get(pURL, {})
+                for link in data['childLinks']:
+                    targetDict[link] = targetDict.get(link, 0) + 1
+                linksDict[pURL] = targetDict
+                if linesCount % 100000 == 0:
+                    print linesCount,
+                    pass
+    tStop = time.time()
+    eTime = tStop - tStart
+    linesPerSec = linesCount / eTime
+    print "lines parsed: ", linesCount, "files to go:", filesCount, "took ", eTime, "; ", linesPerSec, "l/s"
+    writeDictToCsv(fileName, linksDict)  ##needs work here
+
+    linksDict.clear()
+    gc.collect()  #this does not apperar to work; it was included to attempt to free memory
+    print 'done gc'
+
+
+
+##my additions
+##my additions
+##my additions
+##my additions
+##my additions
+
+
+def getFilesInDirectory(inDir):
+    inDir = normpath(inDir)
+    onlyFilesTemp = [normpath(os.path.join(inDir, f)) for f in listdir(inDir) if (isfile(join(inDir, f)))]
+
+    onlyFiles = []
+    #do not return log files
+    for line in onlyFilesTemp:
+        try:
+            if (line[-3:] != 'txt'):
+                onlyFiles.append(line)
+        except:
+            pass
+
+    return onlyFiles
+
+def getProcessedFiles(inFile):
+    inFile = normpath(inFile)
+    processedFiles = []
+    try:
+        with open(inFile) as fp:
+            #processedFiles = fp.readlines()
+            processedFiles = fp.read().splitlines()
+    except:
+        processedFiles = []
+    return processedFiles
+
+def writeLogFile(logFilePath,processedFileName):
+    logFilePath = normpath(logFilePath)
+    processedFileName = normpath(processedFileName)
+    if os.path.isfile(logFilePath) == False:
+        with open(logFilePath,'w+') as fp: #create the log file
+            pass
+
+    with open(logFilePath,'a+') as fp:
+        fp.write(processedFileName + '\n')
+
+
+
+def writeFailedLog(logFilePath,failedFileName):
+    logFilePath = normpath(logFilePath)
+    failedFileName = normpath(failedFileName)
+    if os.path.isfile(logFilePath) == False:
+        with open(logFilePath,'w+') as fp: #create the log file
+            pass
+
+    with open(logFilePath,'a+') as fp:
+        fp.write(failedFileName + '\n')
+
+
+
+
+
+
+
+if __name__=='__main__':
+
+    #note:  There is a memory issue, not a "memory leak"
+    #If a large dictionary is created ~4GB+, memory is consumed by the object
+    #However, when the object goes out of scope, or if it is = None
+    #The memory is not returned
+    #If a subsequent dictionary is created, addtitional memory is allocated
+    #This continues until swap is consumed, etc.
+    #One remedy for this is to operate in a single threaded process
+    #The code below does not actually do anything in parallel, but it uses the multiprocessing
+    #pool to ensure the processing thread is eliminated and memory is returned
+
+    inDir = '/media/user1/Seagate Backup Plus Drive/memexGithub/data/type2'
+    inDir = normpath(inDir)
+
+    #get list of processed files
+    logSuccess = normpath(inDir + normpath('/processedFiles.txt'))
+    logFailed = normpath(inDir + normpath('/failedFiles.txt'))
+    rawFiles = getFilesInDirectory(inDir)
+
+    processedFiles = getProcessedFiles(logSuccess)
+    processedFiles += getProcessedFiles(logFailed)
+
+    filesCount = len(rawFiles)
+
+    for file in rawFiles:
+        if file not in processedFiles:
+            # do the processing here
+            start = time.clock()
+            print "processing " + file
+            try:
+                pool = Pool(processes=1)
+                input = (file,)
+                pool.map(doTheWork, (input,))
+                pool.close()
+                pool.join()
+                gc.collect()
+                eTime = time.clock() - start
+                print str(eTime) + ' sec'
+
+                # logging
+                print 'logging ', file
+                writeLogFile(logSuccess, file)
+                time.sleep(0.1)
+            except:
+                print 'failed; ', file
+                writeFailedLog(logFailed, file)
---
+++ 030ForwardReverseIndices.py
@@ -0,0 +1,172 @@
+import gzip
+import ujson
+import os
+from os import listdir
+from os.path import isfile, join, normpath
+import sys
+import time
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def getFilesInDirectory(inDir):
+    inDir = normpath(inDir)
+    onlyFilesTemp = [normpath(os.path.join(inDir, f)) for f in listdir(inDir) if (isfile(join(inDir, f)))]
+
+    onlyFiles = []
+    #do not return log files
+    for line in onlyFilesTemp:
+        try:
+            if (line[-3:] != 'txt'):
+                onlyFiles.append(line)
+        except:
+            pass
+
+    return onlyFiles
+
+def getProcessedFiles(inFile):
+    inFile = normpath(inFile)
+    processedFiles = []
+    try:
+        with open(inFile) as fp:
+            #processedFiles = fp.readlines()
+            processedFiles = fp.read().splitlines()
+    except:
+        processedFiles = []
+    return processedFiles
+
+def writeLogFile(logFilePath,processedFileName):
+    logFilePath = normpath(logFilePath)
+    processedFileName = normpath(processedFileName)
+    if os.path.isfile(logFilePath) == False:
+        with open(logFilePath,'w+') as fp: #create the log file
+            pass
+
+    with open(logFilePath,'a+') as fp:
+        fp.write(processedFileName + '\n')
+
+
+
+def writeFailedLog(logFilePath,failedFileName):
+    logFilePath = normpath(logFilePath)
+    failedFileName = normpath(failedFileName)
+    if os.path.isfile(logFilePath) == False:
+        with open(logFilePath,'w+') as fp: #create the log file
+            pass
+
+    with open(logFilePath,'a+') as fp:
+        fp.write(failedFileName + '\n')
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def createDictionaries(fileName):
+    linksDict = {}
+    linesCount = 0
+    nameValueDict = {} #this must persist through function calls
+    nameValueMeta={'currentKey':0}
+    with gzip.open(fileName) as fp:
+        #The file is only 1 line
+        tStart = time.time()
+        line = fp.readline()
+
+    data = ujson.loads(line)
+
+    kTemp = data.keys()
+    for pURL in kTemp:
+
+        linesCount += 1
+
+        if pURL in nameValueDict.keys():
+            pass
+            #good
+            #maybe do something with forward reverse
+        else:
+            nameValueDict[pURL]=nameValueMeta[currentKey]
+            nameValueMeta[currentKey] +=1
+
+
+
+
+
+
+
+
+
+    #        if (len(pURL) > 1):
+    #            targetDict = linksDict.get(pURL, {})
+    #            for link in data['childLinks']:
+    #                targetDict[link] = targetDict.get(link, 0) + 1
+    #            linksDict[pURL] = targetDict
+    #            if linesCount % 100000 == 0:
+    #                print linesCount,
+    #                pass
+    #tStop = time.time()
+    #eTime = tStop - tStart
+    #linesPerSec = linesCount / eTime
+    #print "lines parsed: ", linesCount, "files to go:", filesCount, "took ", eTime, "; ", linesPerSec, "l/s"
+    #writeDictToCsv(fileName, linksDict)  ##needs work here
+
+    #linksDict.clear()
+    #gc.collect()  #this does not apperar to work; it was included to attempt to free memory
+    #print 'done gc'
+
+
+
+
+
+
+
+if __name__=='__main__':
+
+
+    inDir = '/media/user1/Seagate Backup Plus Drive/memexGithub/data/type2/counted/'
+    inDir = normpath(inDir)
+
+    #get list of processed files
+    logSuccess = normpath(inDir + normpath('/processedFiles.txt'))
+    logFailed = normpath(inDir + normpath('/failedFiles.txt'))
+    rawFiles = getFilesInDirectory(inDir)
+
+    processedFiles = getProcessedFiles(logSuccess)
+    processedFiles += getProcessedFiles(logFailed)
+
+    filesCount = len(rawFiles)
+
+    for file in rawFiles:
+        if file not in processedFiles:
+            # do the processing here
+            start = time.clock()
+            print "processing " + file
+            try:
+                createDictionaries(file)
+                eTime = time.clock() - start
+                print str(eTime) + ' sec'
+
+                # logging
+                print 'logging ', file
+                #writeLogFile(logSuccess, file)
+                time.sleep(0.1)
+            except:
+                print 'failed; ', file
+                #writeFailedLog(logFailed, file)
+
+
+
+
+
